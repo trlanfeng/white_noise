6a2704347be424fb441f7865002eb3ea18074a65
trlanfeng <trlanfeng@foxmail.com>
2019-11-04 17:19:37 +0800

    页面初始化

---
+++ lib/pages/sound_current.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class SoundCurrent extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SoundCurrentState();
+  }
+}
+
+class SoundCurrentState extends State<SoundCurrent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/sound_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class SoundList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SoundListState();
+  }
+}
+
+class SoundListState extends State<SoundList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
