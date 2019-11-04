01fa5e306956c1894d0a7b239d4d1a92965636de
trlanfeng <trlanfeng@foxmail.com>
2019-11-04 17:21:54 +0800

    增加设置页面

---
+++ lib/pages/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class Settings extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return SettingsState();
+  }
+}
+
+class SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
