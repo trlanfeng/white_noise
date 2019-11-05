9c3247e656bdc1dfc8fd7049b3cba7b9768488a4
trlanfeng <trlanfeng@foxmail.com>
2019-11-05 17:34:13 +0800

    加入资源

--- lib/pages/sound_list.dart
+++ lib/pages/sound_list.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class SoundList extends StatefulWidget {
@@ -10,6 +12,25 @@ class SoundList extends StatefulWidget {
 class SoundListState extends State<SoundList> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CupertinoPageScaffold(
+      backgroundColor: Colors.black,
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: Colors.white,
+        middle: Text(
+          '声音',
+        ),
+      ),
+      child: Container(
+        child: GridView.count(
+          crossAxisCount: 3,
+          children: <Widget>[
+            Container(
+              child: Image.asset('/assets/icons/png/ic_city_airplane_active.png'),
+              // Text('声音'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
